1846e4df2ab19aa48f0849e91b6303b41f7d27ec
gebrina <gsonofmika@gmail.com>
2024-07-19 22:56:10 +0300

    create appliction db context and add it to DI

---
+++ Auth.Domain/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+using Auth.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auth.Domain.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+}
