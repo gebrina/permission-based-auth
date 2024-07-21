b53a0f62e00ffa3f42b439dbf4d03549e9fd72a1
gebrina <gsonofmika@gmail.com>
2024-07-21 16:31:42 +0300

    add validation logic for user password

--- Auth.Api/Validations/CreateUserDtoValidator.cs
+++ Auth.Api/Validations/CreateUserDtoValidator.cs
@@ -11,7 +11,9 @@ public class CreateUserDtoValidator : AbstractValidator<CreateUserDto>
     {
         RuleFor(user => user.Email).EmailAddress().NotEmpty()
         .NotNull();
-        RuleFor(user => user.Password).NotNull().NotEmpty();
+        RuleFor(user => user.Password)
+      .NotNull().NotEmpty().Matches(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).{6,}$")
+      .WithMessage("Password must contain atleast one number, unique character, uppercase letter, lowercase letter and must be 6 or more character length.");
         RuleFor(user => user.UserName).NotNull().NotEmpty();
         RuleFor(user => user.Profession).Matches("^[a-zA-Z]+$")
         .WithMessage("Only letters are allowed for profession");
